c7351e215b799ccf2446f2800fbf11991d541a33
Vladimir <originalblack@mail.ru>
2017-03-20 00:24:17 +0300

    add databese connet class

---
+++ insert_data.py
@@ -0,0 +1,34 @@
+import MySQLdb
+
+class Database:
+
+    host = 'localhost'
+    user = 'root'
+    password = '123456'
+    db = 'test_data'
+
+    def __init__(self):
+        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        self.cursor = self.connection.cursor()
+
+    def insert(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except:
+            self.connection.rollback()
+
+
+
+    def query(self, query):
+        cursor = self.connection.cursor( MySQLdb.cursors.DictCursor )
+        cursor.execute(query)
+
+        return cursor.fetchall()
+     def __del__(self):
+        self.connection.close()
+
+
+if __name__ == "__main__":
+
+    db = Database()
