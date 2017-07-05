b82253f536aa25598433016dbeb65f4f48ced9c5
Vladimir <originalblack@mail.ru>
2017-07-06 00:50:22 +0300

    test

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-int(i) for i in input().split
+# int(i) for i in input().split
+
+import sys
+
+
+while True:
+    my_try = 1
+    num = sys.stdin.read()
+    num.replace('\n', '')
+    int_num = int(num.rstrip())
+    print(int_num + my_try)
+    my_try = my_try + 1
