92c2c6834a9040958dcd2c80c360d1faad342c83
Vladimir <originalblack@mail.ru>
2017-07-06 19:13:30 +0300

    fix input range integer

--- jb_task.py
+++ jb_task.py
@@ -1,7 +1,11 @@
 import sys
 
 num = sys.stdin.read().rstrip()
-if len(num) < 2:
-    print "0"
+int_num = int(num)
+if int_num < 0  or  int_num > 1000000:
+    print "Oooppsss, wrong"
 else:
-    print num[-2]
+    if len(num) < 2:
+        print "0"
+    else:
+        print num[-2]
